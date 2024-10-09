57fdbd4b629735c1cef64e9889df213a8de6b3e8
harshil malaviya <harshilmalaviya@gmail.com>
2024-10-09 09:54:23 +0530

    just Started

---
+++ PROGIGS/src/main/java/com/Ntra/PROGIGS/ProgigsApplication.java
@@ -0,0 +1,13 @@
+package com.Ntra.PROGIGS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgigsApplication.class, args);
+	}
+
+}
